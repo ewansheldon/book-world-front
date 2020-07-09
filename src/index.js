3908dd3ef08223e220b757f1f618f9275661209c
Ewan <ewan.s@cucumberwifi.io>
2020-07-09 17:32:29 +0100

    adds router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Landing from "./components/Landing/Landing.js";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
 
-ReactDOM.render(<Landing />, document.getElementById("root"));
+ReactDOM.render(
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Landing}/>
+        </Switch>
+      </Router>
+    </>,
+    document.getElementById('root')
+);
