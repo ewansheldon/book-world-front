60cca9793608574e8b88a0b0792a7f153dc09fb0
Ewan <ewan.s@cucumberwifi.io>
2020-07-30 13:07:15 +0100

    styles edit book form

--- src/components/Nico/EditBook.js
+++ src/components/Nico/EditBook.js
@@ -3,7 +3,20 @@ import * as PropTypes from 'prop-types';
 import { withCookies } from "react-cookie";
 import { getAllCountries } from "../../api/Requests";
 
-const NewBook = ({ book, cookies }) => {
+const styles = {
+  input: {
+    margin: 5
+  },
+  textarea: {
+    margin: 5,
+    borderWidth: 0.5,
+    resize: 'none',
+    fontSize: 11,
+    fontFamily: 'arial'
+  }
+}
+
+const EditBook = ({ book, cookies }) => {
   const [title, setTitle] = useState(book.title);
   const [author, setAuthor] = useState(book.author);
   const [country, setCountry] = useState(book.country);
@@ -18,41 +31,45 @@ const NewBook = ({ book, cookies }) => {
   const saveBook = e => {
     e.preventDefault();
     console.log(title, author, country);
-    // createBook({title, author, country}, getToken(cookies)).then(book => {
-    //   addBookToList(book);
-    // });
   };
 
   const countryOptions = allCountries.map((country, index) =>
     <option value={country.alpha3Code} key={index}>{country.name}</option>);
 
+  const bookUnchanged = _ => {
+    return title === book.title && author === book.author && country === book.country && description === book.description && thumbnail === book.thumbnail;
+  }
+
+  const bookInvalid = _ => {
+    return !title || !author || !country || !description || !thumbnail;
+  }
+
+  const updateDisabled = _ => {
+    return (bookInvalid() || bookUnchanged())
+  }
+
   return (
     <form onSubmit={saveBook}>
       <div>
-        <label htmlFor="title">Title:</label>
-        <input id="title" type="text" value={title} onChange={e => setTitle(e.target.value)} />
+        <input style={styles.input} id="title" type="text" value={title} onChange={e => setTitle(e.target.value)} />
       </div>
       <div>
-        <label htmlFor="author">Author:</label>
-        <input id="author" type="text" value={author} onChange={e => setAuthor(e.target.value)} />
+        <input style={styles.input} id="author" type="text" value={author} onChange={e => setAuthor(e.target.value)} />
       </div>
       <div>
-        <label htmlFor="country">Country:</label>
-        <select id="country" value={country} onChange={e => setCountry(e.target.value)}>
+        <select style={styles.input} id="country" value={country} onChange={e => setCountry(e.target.value)}>
           <option value=''></option>
           {countryOptions}
         </select>
       </div>
       <div>
-        <label htmlFor="description">Description:</label>
-        <textarea id="description" type="text" rows="4" cols="50" value={description} onChange={e => setDescription(e.target.value)} />
+        <textarea style={styles.textarea} id="description" type="text" rows="4" cols="60" value={description} onChange={e => setDescription(e.target.value)} />
       </div>
       <div>
-        <label htmlFor="thumbnail">Thumbnail:</label>
-        <input id="thumbnail" type="text" value={thumbnail} onChange={e => setThumbnail(e.target.value)} />
+        <input style={styles.input} id="thumbnail" type="text" value={thumbnail} onChange={e => setThumbnail(e.target.value)} />
       </div>
       <div>
-        <input value="Save Book" type="submit" disabled={!title || !author || !country} />
+        <input style={styles.input} value="Save Book" type="submit" disabled={updateDisabled()} />
       </div>
     </form>
   )
@@ -70,8 +87,8 @@ const book = PropTypes.shape({
   thumbnail: PropTypes.string.isRequired
 });
 
-NewBook.propTypes = {
+EditBook.propTypes = {
   book: book.isRequired
 }
 
-export default withCookies(NewBook);
+export default withCookies(EditBook);
